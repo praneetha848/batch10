b8668f54cd3d57bad3fc4b47cc3aa89242a436c0
sai sri <snowflaketraining48@gmail.com>
2025-06-10 13:36:21 +0000

    commit

--- models/staging/orders.sql
+++ models/staging/orders.sql
@@ -6,5 +6,5 @@ WITH tb1 as(
     user_id AS CUST_ID,
     status
 
-    from {{source('datafeed_shared_schema','raw_orders')}})
+    from {{source('datafeed_shared_schema','raw_orders')}}) 
 select * from tb1
---
+++ snapshots/snap_orders_check.sql
@@ -0,0 +1,9 @@
+{% snapshot snap_orders_check %}
+{{config(
+          target_schema='snapshots',
+          strategy='check',
+          unique_key='id',
+check_cols=['user_id','order_date'],) }}
+select * from {{ source('datafeed_shared_schema', 'raw_orders') }} limit 10
+
+{% endsnapshot %}
